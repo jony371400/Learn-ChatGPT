a74c39073e152f07b8a1e70d6fc73f5c0ccff142
Johnny_Chai <Johnny_Chai@wiwynn.com>
2023-02-14 16:11:09 +0800

    20230214-1610

---
+++ LoginAPI/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('users.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_table():
+    conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)')
+    conn.close()
+
+create_table()
+
+@app.route('/register', methods=['POST'])
+def register():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return jsonify({'message': 'Could not verify registration information'}), 401
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (auth.username,))
+    user = cursor.fetchone()
+    if user:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (auth.username, auth.password))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Registration successful'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return jsonify({'message': 'Could not verify login information'}), 401
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (auth.username, auth.password))
+    user = cursor.fetchone()
+    conn.close()
+
+    if not user:
+        return jsonify({'message': 'Incorrect login credentials'}), 401
+
+    return jsonify({'message': 'Login successful'}), 200
+
+@app.route('/users/<username>', methods=['DELETE'])
+def delete_user(username):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+
+    if not user:
+        return jsonify({'message': 'User does not exist'}), 404
+
+    cursor.execute('DELETE FROM users WHERE username = ?', (username,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'User deleted successfully'}), 200
+
+if __name__ == '__main__':
+    app.run()
---
+++ README.md
@@ -0,0 +1,14 @@
+# This Project is used [ChatGPT] to create a Login API Service
+
+## Step
+
+> Give GPT Cmd to execute
+![Image](https://github.com/jony371400/Project-DashboardApp/blob/main/Pic/HomePage.png)
+
+> Get the response
+![Image](https://github.com/jony371400/Project-DashboardApp/blob/main/Pic/HomePage.png)
+
+> Finally , GPT will teach you how to use
+![Image](https://github.com/jony371400/Project-DashboardApp/blob/main/Pic/HomePage.png)
+
+> Done , easy right?
